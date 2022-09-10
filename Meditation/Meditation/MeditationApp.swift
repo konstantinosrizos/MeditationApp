77d2ded4b391ad44accb6f53b10716899e9b8354
konstantinosrizos <rizos.dev@gmail.com>
2022-09-10 17:18:28 +0100

    Creating the project

---
+++ Meditation/Meditation/MeditationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditationApp.swift
+//  Meditation
+//
+//  Created by Konstantinos Rizos on 10/9/22.
+//
+
+import SwiftUI
+
+@main
+struct MeditationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
